c8d76d7d6f1d4a55fb620681022f20373b6ace32
xhrxbyl <abdirirambo@gmail.com>
2024-11-30 17:12:55 +0300

    lab completed

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,4 +1,70 @@
-require ( './helpers.js' );
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+// Functions that modify the `cats` array destructively
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// Functions that return new arrays without modifying the original `cats` array
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+require ( './helpers.js' ); 
 
 describe('index.js', function () {
   describe('cats', function () {
